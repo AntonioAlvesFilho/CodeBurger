423fb526020dbed658c8cc32243deb8c657538ab
Antonio Alves <antonioalves987654321@gmail.com>
2022-04-05 12:49:40 -0300

    Updates

---
+++ precache-manifest.ab59effc4de5ca8616932361a2305297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6583ada516d302606243",
+    "url": "/CodeBurger/static/js/main.6583ada5.chunk.js"
+  },
+  {
+    "revision": "be704f3cef72d2d5892b",
+    "url": "/CodeBurger/static/js/1.be704f3c.chunk.js"
+  },
+  {
+    "revision": "a52e7ebd00e0e4b03eee",
+    "url": "/CodeBurger/static/js/runtime~main.a52e7ebd.js"
+  },
+  {
+    "revision": "fe9aacf7175fcaba817f510ae217dc7b",
+    "url": "/CodeBurger/static/media/logoHome.fe9aacf7.png"
+  },
+  {
+    "revision": "08dba6bca43d6377f05f50f3fc1fd472",
+    "url": "/CodeBurger/static/media/logoOrders.08dba6bc.png"
+  },
+  {
+    "revision": "63aeb6a30df1dac196cb69f3e2ea3d1b",
+    "url": "/CodeBurger/index.html"
+  }
+];
